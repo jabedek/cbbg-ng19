21aa14889ec176993031a5578344185a94089298
jabedek <jabedek@gmail.com>
2025-03-18 15:23:46 +0100

    secret

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,23 +9,14 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { FIRE_CONFIG } from 'firebase-cred-ignore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZoneChangeDetection({ eventCoalescing: true }),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'cbbg-2024',
-        appId: '1:592100183356:web:448a20fd1027b022742239',
-        storageBucket: 'cbbg-2024.firebasestorage.app',
-        apiKey: 'AIzaSyCSb2u138CcMlWqHWMqses0BdLfzyTwySY',
-        authDomain: 'cbbg-2024.firebaseapp.com',
-        messagingSenderId: '592100183356',
-        measurementId: 'G-QGYNFST8PN',
-      }),
-    ),
+    provideFirebaseApp(() => initializeApp(FIRE_CONFIG)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
   ],
